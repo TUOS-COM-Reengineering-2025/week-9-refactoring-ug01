567edd9716a3dbc1f0eb2595b72d01e3bd865130
COM-J-Badger <JBadger1@sheffield.ac.uk>
2025-05-02 14:17:51 +0100

    remove calculate_shipping_fee_for_heavy_items

--- main.py
+++ main.py
@@ -54,12 +54,6 @@ class CustomerManager:
         else:
             return 20
 
-def calculate_shipping_fee_for_heavy_items(purchases):
-    for purchase in purchases:
-        if purchase.get('weight', 0) > 20:
-            return 50
-    return 20
-
 def calculate_shipping_fee_for_fragile_items(purchases):
     fragile_item = False
     for purchase in purchases:
